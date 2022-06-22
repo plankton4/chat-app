b0dc41c1c569cb1409c6c8cb5849f4c487a16a2b
Dmitry Iv <plankton4.rus@gmail.com>
2022-06-22 22:05:09 +0800

    project creation

---
+++ chatapp/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  chatapp
+//
+//  Created by Dmitry Iv on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
